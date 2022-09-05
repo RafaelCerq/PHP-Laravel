f74ef3f5cea1ce1723ebcbd2cf0714129dd268ec
RafaelCerq <rafaelcerq91@gmail.com>
2022-09-05 16:37:44 -0300

    Protegendo tela de Usuários

--- bolao/routes/web.php
+++ bolao/routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('admin')->namespace('Admin')->group(function () {
+Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function () {
     Route::resource('/users', 'UserController');
 
 });
