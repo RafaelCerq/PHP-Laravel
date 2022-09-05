dd66d3e2968795cce026952992eee34da509660e
RafaelCerq <rafaelcerq91@gmail.com>
2022-09-05 17:52:03 -0300

    Ajustes form

--- bolao/resources/views/admin/users/create.blade.php
+++ bolao/resources/views/admin/users/create.blade.php
@@ -12,25 +12,8 @@
 
 
 
-        @form_component(['action'=>$routeName.".store", 'method'=>"POST"])
-            <div class="row">
-                <div class="form-group col-6">
-                    <label for="name">Nome</label>
-                    <input type="text" class="form-control" name="name" value="">
-                </div>
-                <div class="form-group col-6">
-                    <label for="email">E-Mail</label>
-                    <input type="mail" class="form-control" name="email" value="">
-                </div>
-                <div class="form-group col-6">
-                    <label for="password">Senha</label>
-                    <input type="password" class="form-control" name="password" value="">
-                </div>
-                <div class="form-group col-6">
-                    <label for="password_confirmation">Confirmar Senha</label>
-                    <input type="password" class="form-control" name="password_confirmation" value="">
-                </div>
-            </div>
+        @form_component(['action'=>route($routeName.".store"), 'method'=>"POST"])
+            @include('admin.users.form')
             <button class="btn btn-primary btn-md float-right">Adicionar</button>
         @endform_component
 
---
+++ bolao/resources/views/admin/users/form.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="form-group col-6">
+        <label for="name">Nome</label>
+        <input type="text" class="form-control" name="name" value="">
+    </div>
+    <div class="form-group col-6">
+        <label for="email">E-Mail</label>
+        <input type="mail" class="form-control" name="email" value="">
+    </div>
+    <div class="form-group col-6">
+        <label for="password">Senha</label>
+        <input type="password" class="form-control" name="password" value="">
+    </div>
+    <div class="form-group col-6">
+        <label for="password_confirmation">Confirmar Senha</label>
+        <input type="password" class="form-control" name="password_confirmation" value="">
+    </div>
+</div>
