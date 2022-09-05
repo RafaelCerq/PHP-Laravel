4efb43055881af9a22f2fe790e394400c3548cf4
RafaelCerq <rafaelcerq91@gmail.com>
2022-09-05 17:45:30 -0300

    Formulário com Bootstrap 4

--- bolao/resources/views/admin/users/create.blade.php
+++ bolao/resources/views/admin/users/create.blade.php
@@ -13,7 +13,25 @@
 
 
         @form_component(['action'=>$routeName.".store", 'method'=>"POST"])
-            <input type="text" name="" placeholder="Nome">
+            <div class="row">
+                <div class="form-group col-6">
+                    <label for="name">Nome</label>
+                    <input type="text" class="form-control" name="name" value="">
+                </div>
+                <div class="form-group col-6">
+                    <label for="email">E-Mail</label>
+                    <input type="mail" class="form-control" name="email" value="">
+                </div>
+                <div class="form-group col-6">
+                    <label for="password">Senha</label>
+                    <input type="password" class="form-control" name="password" value="">
+                </div>
+                <div class="form-group col-6">
+                    <label for="password_confirmation">Confirmar Senha</label>
+                    <input type="password" class="form-control" name="password_confirmation" value="">
+                </div>
+            </div>
+            <button class="btn btn-primary btn-md float-right">Adicionar</button>
         @endform_component
 
     @endpage_component
