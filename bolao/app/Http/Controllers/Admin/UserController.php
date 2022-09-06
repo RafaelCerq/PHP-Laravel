dd527647ed595552bf236f5d67ffe0a0043837bf
RafaelCerq <rafaelcerq91@gmail.com>
2022-09-06 00:56:12 -0300

    validação de formulário

--- bolao/app/Http/Controllers/Admin/UserController.php
+++ bolao/app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Contracts\UserRepositoryInterface;
+use Validator;
 
 class UserController extends Controller
 {
@@ -79,7 +80,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ])->validate();
     }
 
     /**
--- bolao/resources/views/admin/users/form.blade.php
+++ bolao/resources/views/admin/users/form.blade.php
@@ -1,15 +1,30 @@
 <div class="row">
     <div class="form-group col-6">
         <label for="name">Nome</label>
-        <input type="text" class="form-control" name="name" value="">
+        <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}">
+        @if ($errors->has('name'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
+        @endif
     </div>
     <div class="form-group col-6">
         <label for="email">E-Mail</label>
-        <input type="mail" class="form-control" name="email" value="">
+        <input type="mail" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
+        @if ($errors->has('email'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+        @endif
     </div>
     <div class="form-group col-6">
         <label for="password">Senha</label>
-        <input type="password" class="form-control" name="password" value="">
+        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="">
+        @if ($errors->has('password'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+        @endif
     </div>
     <div class="form-group col-6">
         <label for="password_confirmation">Confirmar Senha</label>
