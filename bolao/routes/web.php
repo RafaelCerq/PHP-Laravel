ed38078e90953da133eeae48d363de40022dae2c
Rafael <rafaelcerq91@gmail.com>
2022-09-07 23:58:47 -0300

    Finalizando CRUD Permissões

--- bolao/routes/web.php
+++ bolao/routes/web.php
@@ -21,6 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function () {
     Route::resource('/users', 'UserController');
+    Route::resource('/permissions', 'PermissionController');
 
 });
 
