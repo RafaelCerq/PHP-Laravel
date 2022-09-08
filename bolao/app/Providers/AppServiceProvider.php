8d0795010173920feb2127ef3f0527e464214b83
Rafael <rafaelcerq91@gmail.com>
2022-09-07 23:38:07 -0300

    Repository para Permissão

--- bolao/app/Providers/AppServiceProvider.php
+++ bolao/app/Providers/AppServiceProvider.php
@@ -32,5 +32,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('App\Repositories\Contracts\UserRepositoryInterface', 'App\Repositories\Eloquent\UserRepository');
+        $this->app->bind('App\Repositories\Contracts\PermissionRepositoryInterface', 'App\Repositories\Eloquent\PermissionRepository');
     }
 }
