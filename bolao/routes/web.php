6c790a5e4b2126ec42d4c25eb8c8689f13099158
Rafael <rafaelcerq91@gmail.com>
2022-09-09 01:20:57 -0300

    Ajuste Rotas

--- bolao/routes/web.php
+++ bolao/routes/web.php
@@ -17,7 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->namespace('Admin')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
 
 Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function () {
     Route::resource('/users', 'UserController');
