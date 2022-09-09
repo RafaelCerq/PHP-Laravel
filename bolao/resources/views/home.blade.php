3248d9d3e9904bb59be2154aad1c84f642971716
RafaelCerq <rafaelcerq91@gmail.com>
2022-09-09 17:28:27 -0300

    Ajustando cards painel de controle

--- bolao/resources/views/home.blade.php
+++ bolao/resources/views/home.blade.php
@@ -2,37 +2,61 @@
 
 @section('content')
 
-    @page_component(['col'=>8, 'page'=>$page])
+    @page_component(['col'=>12, 'page'=>$page])
 
         @alert_component(['msg'=>session('msg'), 'status'=>session('status')])
         @endalert_component
 
-        <div class="row">
+        <div id="portfolio">
+            <div class="row">
+                @can ('list-users')
+                    <div style="cursor:pointer" onclick="window.location = '{{route('users.index')}}'" class="col-md-4 col-sm-6 portfolio-item">
+                        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                            <div class="portfolio-hover">
+                                <div class="portfolio-hover-content">
+                                    <i class="fas fa-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img class="img-fluid" src="img/portfolio/02-thumbnail.jpg" alt="">
+                        </a>
+                        <div class="portfolio-caption">
+                            <h4>@lang('bolao.list',['page'=>__('bolao.user_list')])</h4>
+                            <p class="text-muted">@lang('bolao.create_or_edit')</p>
+                        </div>
+                    </div>
+                @endcan
 
-            @can ('list-users')
-            <div style="cursor:pointer" onclick="window.location = '{{route('users.index')}}'" class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header">@lang('bolao.list',['page'=>__('bolao.user_list')])</div>
-                <div class="card-body">
-                    <p class="card-text">@lang('bolao.create_or_edit')</p>
+                <div style="cursor:pointer" onclick="window.location = '{{route('permissions.index')}}'" class="col-md-4 col-sm-6 portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fas fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="img/portfolio/permissao.jpg" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>@lang('bolao.list',['page'=>__('bolao.permission_list')])</h4>
+                        <p class="text-muted">@lang('bolao.create_or_edit')</p>
+                    </div>
                 </div>
-            </div>
-            @endcan
 
-
-            <div style="cursor:pointer" onclick="window.location = '{{route('permissions.index')}}'" class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-                <div class="card-header">@lang('bolao.list',['page'=>__('bolao.permission_list')])</div>
-                <div class="card-body">
-                    <p class="card-text">@lang('bolao.create_or_edit')</p>
+                <div style="cursor:pointer" onclick="window.location = '{{route('roles.index')}}'" class="col-md-4 col-sm-6 portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fas fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="img/portfolio/05-thumbnail.jpg" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>@lang('bolao.list',['page'=>__('bolao.role_list')])</h4>
+                        <p class="text-muted">@lang('bolao.create_or_edit')</p>
+                    </div>
                 </div>
-            </div>
 
-            <div style="cursor:pointer" onclick="window.location = '{{route('roles.index')}}'" class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                <div class="card-header">@lang('bolao.list',['page'=>__('bolao.role_list')])</div>
-                <div class="card-body">
-                    <p class="card-text">@lang('bolao.create_or_edit')</p>
-                </div>
             </div>
-
         </div>
 
     @endpage_component
