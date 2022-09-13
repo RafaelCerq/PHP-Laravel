b7d97f68dbd4a3dc292df3270811e2ccbff490c5
RafaelCerq <rafaelcerq91@gmail.com>
2022-09-13 11:55:14 -0300

    Repository para Rodadas

---
+++ bolao/app/Repositories/Eloquent/RoundRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Round;
+use App\Repositories\Contracts\RoundRepositoryInterface;
+
+class RoundRepository extends AbstractRepository implements RoundRepositoryInterface
+{
+
+    protected $model = Round::class;
+
+}
